13c96d3611d66c0cb74f0ef24543ca03dc7a9dd7
Akashdeep Jaswal <akashdeepsinghjaswal@gmail.com>
2016-05-31 05:00:15 -0700

    Final
    Final

--- Data Cleaning Scripts/gather_parts.py
+++ Data Cleaning Scripts/gather_parts.py
@@ -1,7 +1,8 @@
 import graphlab as gl
 
 all_wiki_data = gl.SFrame.read_csv('/Users/Akash/Desktop/wikitrends-aws-data/*.csv', header = True)
+all_wiki_data.save("../Data/Raw_Wiki_2010_15.csv")
 
-grouped_data = all_wiki_data.groupby(["Proj","Page","Date"],gl.aggregate.SUM('PageViews'))
-print grouped_data
-grouped_data.save("All_Wiki_2010_15.csv")
+# grouped_data = all_wiki_data.groupby(["Proj","Page","Date"],gl.aggregate.SUM('PageViews'))
+# print grouped_data
+# grouped_data.save("All_Wiki_2010_15.csv")
